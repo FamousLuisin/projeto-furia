f35ae6668c276755b10149c7f5f977ebd74edd05
Luís Filipe <lufimelo@outlook.com>
2025-04-29 14:26:44 -0300

    chore: initial Spring Boot project setup

---
+++ api/src/test/java/com/furia/challenge/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.furia.challenge.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
